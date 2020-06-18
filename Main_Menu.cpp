a5433fb7fc2ac34ed46e48e5eccc3f5ad4ec520e
Qbertio <66698583+Qbertio@users.noreply.github.com>
2020-06-17 23:53:59 -0700

    Add files via upload

--- Main_Menu.cpp
+++ Main_Menu.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************************
+* Programmer: Barnwell, Padgett, Terry, Ye													*
+* Class: CptS 122, Summer, 2020; Lab Section 1												*
+* Programming Assignment: PA 6																*
+* Date: June 17, 2020																		*
+* Description: This program runs a graphic application of the video game Tetris	with added  *
+*colored blocks, point system, 1 and 2 player game modes, and demonstrates class inheritance*
+*and polymorphism.																			*
+********************************************************************************************/
+
 #include "Main_Menu.h"
 
 int Main_Menu::twoPlayerMenu()
